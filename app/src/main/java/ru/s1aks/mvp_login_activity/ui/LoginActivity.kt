8bfb3c3bbb55ebb5cdf7cb8e2244b8b6c7d88466
Viacheslav Khrykin <vh78aks@gmail.com>
2021-09-28 21:15:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/s1aks/mvp_login_activity/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ru.s1aks.mvp_login_activity.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.s1aks.mvp_login_activity.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
